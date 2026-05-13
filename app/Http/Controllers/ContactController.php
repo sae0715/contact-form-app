628e86a48937d55171742601ea2524847e4bc87a
saeko inamine <saeco0715@gmail.com>
2026-05-13 10:40:21 +0900

    問い合わせ画面タグ追加

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,19 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Tag;
 
 class ContactController extends Controller
 {
     public function index()
     {
         $categories = Category::all();
-        return view('contact.index', ['categories' => $categories]);
+        $tags = Tag::all();
+
+        return view('contact.index', [
+            'categories' => $categories,
+            'tags' => $tags,
+        ]);
     }
 
     public function confirm(Request $request)
     {
         $categories = Category::all();
-        return view('contact.confirm', $request->all());
+        $category = Category::find($request->input('category_id'));
+
+        return view('contact.confirm', [
+            'categories' => $categories,
+            'validated' => $request->all(),
+            'category' => $category,
+        ]);
     }
 
     public function thanks()
